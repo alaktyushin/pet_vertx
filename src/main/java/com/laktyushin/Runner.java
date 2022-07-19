a912b911f075b48ac15443310aa7a4fd035bdbe7
Andrey N Laktyushin <andrey@laktyushin.com>
2022-07-19 13:13:13 +0300

    Runner

--- src/main/java/com/laktyushin/Runner.java
+++ src/main/java/com/laktyushin/Runner.java
@@ -1,4 +1,17 @@
 package com.laktyushin;
 
-public class Runner {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final public class Runner {
+    private static final Logger LOG = LoggerFactory.getLogger(Runner.class);
+
+    public static void main(String[] args) {
+        LOG.info("Starting Runner");
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> LOG.error("Uncaught exception {} on thread {} ", e, t.getName()));
+        //AnnotationConfigApplicationContext springContext = new AnnotationConfigApplicationContext();
+        //LOG.info("active profiles are: {}, ", Arrays.toString(springContext.getEnvironment().getActiveProfiles()));
+        //springContext.register(ValaRunnerConfiguration.class);
+        //springContext.refresh();
+    }
 }
