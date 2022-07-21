685d4add172e501a3f3640ee47828bc72d18e42b
Andrey N Laktyushin <andrey@laktyushin.com>
2022-07-21 21:01:23 +0300

    1.02.1

--- src/main/java/com/laktyushin/verticles/MathVerticle.java
+++ src/main/java/com/laktyushin/verticles/MathVerticle.java
@@ -6,6 +6,10 @@ import com.laktyushin.messages.MathMessage;
 import com.laktyushin.messages.MathMethods;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 
 public class MathVerticle extends AbstractVerticle {
 
@@ -25,5 +29,39 @@ public class MathVerticle extends AbstractVerticle {
         MathMessage message = new MathMessage(x, y, mathMessage);
         vertx.eventBus().registerDefaultCodec(MathMessage.class, new Codec<>());
         vertx.eventBus().send("LoggingVerticle", message);
+
+        HttpServer server = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+
+        router.route().method(HttpMethod.POST).path("/exp*").handler(ctx -> {
+
+            HttpServerResponse response = ctx.response();
+            response.putHeader("content-type", "text/plain");
+            response.end(String.valueOf(ctx.request()));
+        });
+
+        router.route().method(HttpMethod.GET).path("/exp*").handler(ctx -> {
+            HttpServerResponse response = ctx.response();
+            response.putHeader("content-type", "text/plain");
+            response.end("GET");
+        });
+
+        router.route()
+                .path("/math")
+                .handler(ctx -> {
+                    HttpServerResponse response = ctx.response().setChunked(true);
+                    response.putHeader("content-type", "text/plain");
+                    response.end("X = "
+                            .concat(String.valueOf(x))
+                            .concat("    Y = ")
+                            .concat(String.valueOf(y))
+                            .concat(System.lineSeparator())
+                            .concat(System.lineSeparator())
+                            .concat(mathMessage)
+                    );
+                });
+
+        server.requestHandler(router).listen(8081);
     }
 }
