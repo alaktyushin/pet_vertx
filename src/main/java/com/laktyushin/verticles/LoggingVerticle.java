8576ddcac47ffdf764a602adb3f0738ffc6f06de
Andrey N Laktyushin <andrey@laktyushin.com>
2022-07-22 12:31:42 +0300

    1.03

--- src/main/java/com/laktyushin/verticles/LoggingVerticle.java
+++ src/main/java/com/laktyushin/verticles/LoggingVerticle.java
@@ -1,11 +1,13 @@
 package com.laktyushin.verticles;
 
 import com.laktyushin.Starter;
-import com.laktyushin.messages.MathMessage;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 import static com.laktyushin.Starter.LOG;
 
@@ -13,16 +15,20 @@ public class LoggingVerticle extends AbstractVerticle {
     @Override
     public void start(Promise<Void> startPromise) {
         Starter.LOG.info("LoggingVerticle deployed");
+        Router router = Router.router(vertx);
+        HttpServer server = vertx.createHttpServer();
+        server.requestHandler(router).listen(8080);
+        AtomicReference<Object> body = new AtomicReference<>();
         vertx.eventBus().consumer("LoggingVerticle", message -> {
-            MathMessage msg = (MathMessage) message.body();
-            LOG.info("I have received a message: " + msg);
-            HttpServer server = vertx.createHttpServer();
-            server.requestHandler(request -> {
-                HttpServerResponse response = request.response();
-                response.putHeader("content-type", "text/plain");
-                response.end(message.body().toString());
-            });
-            server.listen(8080);
+                    LOG.info("I have received a message.");
+                    message.reply("Message delivered.");
+                    body.set(message.body());
+                }
+        );
+        router.route().handler(ctx -> {
+            HttpServerResponse response = ctx.response();
+            response.putHeader("content-type", "text/plain");
+            response.end(body.get().toString());
         });
     }
 }
--- src/main/java/com/laktyushin/verticles/MathVerticle.java
+++ src/main/java/com/laktyushin/verticles/MathVerticle.java
@@ -6,7 +6,7 @@ import com.laktyushin.messages.MathMessage;
 import com.laktyushin.messages.MathMethods;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.http.HttpMethod;
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
@@ -23,45 +23,50 @@ public class MathVerticle extends AbstractVerticle {
 
     @Override
     public void start(Promise<Void> startPromise) {
+        EventBus eb = vertx.eventBus();
         Starter.LOG.info("MathVerticle deployed");
-        MathMethods mathMethods = new MathMethods();
-        String mathMessage = mathMethods.getStringBuilder(x, y).toString();
-        MathMessage message = new MathMessage(x, y, mathMessage);
-        vertx.eventBus().registerDefaultCodec(MathMessage.class, new Codec<>());
-        vertx.eventBus().send("LoggingVerticle", message);
-
-        HttpServer server = vertx.createHttpServer();
-
+        final MathMethods[] mathMethods = {new MathMethods()};
+        final String[] mathMessage = {mathMethods[0].getStringBuilder(x, y).toString()};
+        final MathMessage[] message = {new MathMessage(x, y, mathMessage[0])};
+        eb.registerDefaultCodec(MathMessage.class, new Codec<>());
         Router router = Router.router(vertx);
-
-        router.route().method(HttpMethod.POST).path("/exp*").handler(ctx -> {
-
-            HttpServerResponse response = ctx.response();
-            response.putHeader("content-type", "text/plain");
-            response.end(String.valueOf(ctx.request()));
-        });
-
-        router.route().method(HttpMethod.GET).path("/exp*").handler(ctx -> {
-            HttpServerResponse response = ctx.response();
-            response.putHeader("content-type", "text/plain");
-            response.end("GET");
-        });
-
-        router.route()
-                .path("/math")
-                .handler(ctx -> {
-                    HttpServerResponse response = ctx.response().setChunked(true);
-                    response.putHeader("content-type", "text/plain");
-                    response.end("X = "
-                            .concat(String.valueOf(x))
-                            .concat("    Y = ")
-                            .concat(String.valueOf(y))
-                            .concat(System.lineSeparator())
-                            .concat(System.lineSeparator())
-                            .concat(mathMessage)
-                    );
-                });
-
+        HttpServer server = vertx.createHttpServer();
         server.requestHandler(router).listen(8081);
+        eb.request("LoggingVerticle", message[0], res -> {
+            if (res.succeeded()) {
+                Starter.LOG.info("Received reply: " + res.result().body());
+            }
+        });
+        router
+                .get("/math")
+                .handler(routingContext -> {
+                    String unparsedX = routingContext.queryParam("X").get(0);
+                    String unparsedY = routingContext.queryParam("Y").get(0);
+                    if ((unparsedX == null) || (unparsedY == null)) {
+                        routingContext.fail(400);
+                        return;
+                    }
+                    double x;
+                    double y;
+                    try {
+                        x = Integer.parseInt(unparsedX);
+                        y = Integer.parseInt(unparsedY);
+                    } catch (NumberFormatException e) {
+                        routingContext.fail(401, e);
+                        return;
+                    }
+                    Starter.LOG.info("X = " + x);
+                    Starter.LOG.info("Y = " + y);
+                    mathMessage[0] = mathMethods[0].getStringBuilder(x, y).toString();
+                    message[0] = new MathMessage(x, y, mathMessage[0]);
+                    eb.request("LoggingVerticle", message[0], res -> {
+                        if (res.succeeded()) {
+                            Starter.LOG.info("Received reply: " + res.result().body());
+                        }
+                    });
+                    HttpServerResponse response = routingContext.response();
+                    response.putHeader("content-type", "text/plain");
+                    response.end("Received GET, with parameters " + x + ", " + y);
+                });
     }
 }
